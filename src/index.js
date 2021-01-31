bb640dc3af6fac32dd9f620cbae2d8ea40280479
Podexus23 <santon23@gmail.com>
2021-02-01 01:11:36 +0300

    feat: finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if(Array.isArray(array) == false || array.length == 0) {return 0}
 
-exports.max = function max (array) {
-  return 0;
-}
+    return array.reduce((a,b) => {
+      return (a<b)?a:b;
+    });}
+
+
+exports.max = function min (array) {
+    if(Array.isArray(array) == false || array.length == 0) {return 0}
+    return array.reduce((a,b) => {
+      return (a>b)?a:b;
+    });}
+
 
 exports.avg = function avg (array) {
-  return 0;
+    if(Array.isArray(array) == false || array.length == 0) {return 0}
+    return array.reduce((a,b)=> a + b)/array.length;
 }
